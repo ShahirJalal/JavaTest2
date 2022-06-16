7bc03229f6f6e0cd72fb5d2dc7a479599d074c65
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-16 21:39:51 +0800

    Update

---
+++ Test56/MyFrame.java
@@ -0,0 +1,23 @@
+package Test56;
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+    
+    MyFrame() {
+
+        this.setTitle("QUBIKOM.exe");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(420,420);
+        this.setVisible(true);
+    
+        ImageIcon image = new ImageIcon("Pauli.png");
+        this.setIconImage(image.getImage());
+        this.getContentPane().setBackground(new Color(0,0,0));
+        
+    }
+
+}
---
+++ Test56/Test56.java
@@ -0,0 +1,26 @@
+package Test56;
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Test56 {
+    public static void main(String[] args) {
+        
+        // JFrame = a GUI window to add components to
+
+        // JFrame frame = new JFrame();
+        // frame.setTitle("QUBIKOM.exe");
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setResizable(false);
+        // frame.setSize(420,420);
+        // frame.setVisible(true);
+
+        // ImageIcon image = new ImageIcon("Pauli.png");
+        // frame.setIconImage(image.getImage());
+        // frame.getContentPane().setBackground(new Color(0,0,0));
+
+        MyFrame myFrame = new MyFrame();
+        
+    }
+}
