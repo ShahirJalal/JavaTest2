f52c43f0e36c2577f274a1c89a0021357632315c
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-16 18:31:34 +0800

    Update Test55.java

--- Test55.java
+++ Test55.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class Test55 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
         
+        Scanner scanner = new Scanner(System.in);
+
+        File file = new File("Play_Dead.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        clip.start();
+
+        String response = scanner.next();
+
     }
 }
