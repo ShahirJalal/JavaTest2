0e8c513ccd8c4b37a235a937b133921cfc557f03
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-17 20:27:57 +0800

    Update

--- Test58/Test58.java
+++ Test58/Test58.java
@@ -12,30 +12,31 @@ public class Test58 {
          
         // JPanel = a GUI component that function as a container to hold other components
 
-        ImageIcon icon = new ImageIcon("Test58/Thumbs_Up.png");
+        ImageIcon icon = new ImageIcon("Test58/Rocket.png");
 
         JLabel label = new JLabel();
-        label.setText("Hello World");
+        label.setText("Hello World!");
         label.setIcon(icon);
 
         JPanel redPanel = new JPanel();
-        redPanel.setBackground(Color.white);
+        redPanel.setBackground(Color.blue);
         redPanel.setBounds(0, 0, 250, 250);
 
         JPanel bluePanel = new JPanel();
-        bluePanel.setBackground(Color.black);
+        bluePanel.setBackground(Color.red);
         bluePanel.setBounds(250,0,250, 250);
 
         JPanel greenPanel = new JPanel();
         greenPanel.setBackground(Color.yellow);
         greenPanel.setBounds(0, 250, 500, 250);
+        //greenPanel.setLayout(new BorderLayout());
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setSize(750,750);
         frame.setVisible(true);
-        redPanel.add(label);
+        greenPanel.add(label);
         frame.add(redPanel);
         frame.add(bluePanel);
         frame.add(greenPanel);
