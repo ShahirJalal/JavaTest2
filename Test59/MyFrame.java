d86069c6bf3e02ef6188109cbe532d99b173088a
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-18 12:55:50 +0800

    Update MyFrame.java

--- Test59/MyFrame.java
+++ Test59/MyFrame.java
@@ -1,5 +1,13 @@
 package Test59;
 
+import javax.swing.JFrame;
+
 public class MyFrame extends Main {
     
+    MyFrame() {
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        
+    }
 }
