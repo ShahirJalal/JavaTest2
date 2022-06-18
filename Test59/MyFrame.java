eb319066f0874518b4528d3324ef580f3badaf32
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-18 12:56:28 +0800

    Update MyFrame.java

--- Test59/MyFrame.java
+++ Test59/MyFrame.java
@@ -8,6 +8,7 @@ public class MyFrame extends Main {
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
-        
+        this.setSize(500, 500);
+        this.setVisible(true);
     }
 }
