e41ccbfb3b54960b398b411e005dddfd13b9175e
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-18 13:04:08 +0800

    Update

--- Test59/Main.java
+++ Test59/Main.java
@@ -3,6 +3,8 @@ package Test59;
 public class Main {
     public static void main(String[] args) {
         
-        MyFrame frame = new MyFrame();
+        //JButton = a button that performs an action when clicked on
+
+        new MyFrame();
     }
 }
--- Test59/MyFrame.java
+++ Test59/MyFrame.java
@@ -1,14 +1,28 @@
 package Test59;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MyFrame extends Main {
+public class MyFrame extends JFrame implements ActionListener{
     
     MyFrame() {
+
+        JButton button = new JButton();
+        button.setBounds(200, 100, 100, 50);
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500, 500);
         this.setVisible(true);
+        this.add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        
     }
 }
