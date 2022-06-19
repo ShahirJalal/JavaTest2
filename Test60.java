2c4eb8430b07dc13a212ee2981f922e930cf6f69
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-19 21:25:39 +0800

    Update Test60.java

--- Test60.java
+++ Test60.java
@@ -1,5 +1,8 @@
 public class Test60 {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
     
     }
 }
