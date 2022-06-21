18803572a1bce01e26a28fc0ec3c06d09ff929f6
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-21 23:47:37 +0800

    Create Test63.java

---
+++ Test63.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Test63 {
+    
+    public static void main(String[] args) {
+
+        // JLayeredPane = Swing container that provides a
+        //                third dimension for positioning components
+        //                ex. depth, Z-index
+
+        JFrame frame = new JFrame("JLayeredPane");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(500, 500));
+        frame.setLayout(null);
+        frame.setVisible(b);
+        
+    }
+}
