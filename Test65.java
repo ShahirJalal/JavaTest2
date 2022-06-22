fbbc072c0241752964cbbb7c4781315100f84e7d
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-22 23:36:19 +0800

    Create Test65.java

---
+++ Test65.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Test65 {
+    
+    public static void main(String[] args) {
+        
+        // JOptionPane = pop up a standard dialog box that prompts users for a value
+        //               informs them of something.
+
+        JOptionPane.showMessageDialog(parentComponent, message, title, messageType);
+
+
+    }
+}
