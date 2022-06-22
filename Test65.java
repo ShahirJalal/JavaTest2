b2fa4113712d5c13f1558fd94bd3c28ce04c8840
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-22 23:47:18 +0800

    Update Test65.java

--- Test65.java
+++ Test65.java
@@ -8,11 +8,12 @@ public class Test65 {
         //               informs them of something.
 
         //JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.PLAIN_MESSAGE);
-        JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.INFORMATION_MESSAGE);
+        //JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.INFORMATION_MESSAGE);
         //JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.QUESTION_MESSAGE);
         //JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.WARNING_MESSAGE);
         //JOptionPane.showMessageDialog(null, "This is some useless info", "title", JOptionPane.ERROR_MESSAGE);
 
+        int answer = JOptionPane.showConfirmDialog(null, "bro, do you even code?", "This is my title", JOptionPane.YES_NO_CANCEL_OPTION)
 
     }
 }
