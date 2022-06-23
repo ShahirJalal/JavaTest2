addbf7f08ced758cf5adf06850c6bf1b1086de31
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-23 23:56:46 +0800

    Update MyFrame.java

--- Test66/MyFrame.java
+++ Test66/MyFrame.java
@@ -23,3 +23,28 @@ public class MyFrame extends JFrame implements ActionListener{
 		button = new JButton("Submit");
 		button.addActionListener(this);
 		
+		textField = new JTextField();
+		textField.setPreferredSize(new Dimension(250,40));
+		textField.setFont(new Font("Consolas",Font.PLAIN,35));
+		textField.setForeground(new Color(0x00FF00));
+		textField.setBackground(Color.black);
+		textField.setCaretColor(Color.white);
+		textField.setText("username");
+		
+		
+		this.add(button);
+		this.add(textField);
+		this.pack();
+		this.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==button) {
+			System.out.println("Welcome "+ textField.getText());
+			//button.setEnabled(false);
+			//textField.setEditable(false);
+		}
+		
+	}
+}
+// *******************************************************
