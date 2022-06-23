a41bac734ef6efd63601dcb9dae8a3f534d8e0eb
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-23 23:55:48 +0800

    Update

--- Test66/Main.java
+++ Test66/Main.java
@@ -1,3 +1,5 @@
+package Test66;
+
 public class Main{
 
 	public static void main(String[] args) {
---
+++ Test66/MyFrame.java
@@ -0,0 +1,13 @@
+package Test66;
+
+// *******************************************************
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
