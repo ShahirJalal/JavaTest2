2dd3abeb312e7d05d42e4492bf17ba74bde5dfad
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-24 18:18:04 +0800

    Update MyFrame.java

--- Test68/MyFrame.java
+++ Test68/MyFrame.java
@@ -8,13 +8,17 @@ import javax.swing.JFrame;
 
 public class MyFrame extends JFrame implements ActionListener {
 
+    JRadioButton pizzaButton;
+    JRadioButton hamburgerButton;
+    JRadioButton hotdogButton;
+
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
-        JRadioButton pizzaButton = new JRadioButton("pizza");
-        JRadioButton hamburgerButton = new JRadioButton("hamburger");
-        JRadioButton hotdogButton = new JRadioButton("hotdog");
+        pizzaButton = new JRadioButton("pizza");
+        hamburgerButton = new JRadioButton("hamburger");
+        hotdogButton = new JRadioButton("hotdog");
 
         ButtonGroup group = new ButtonGroup();
         group.add(pizzaButton);
@@ -31,7 +35,13 @@ public class MyFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == pizzaButton) {
-            
+            System.out.println("You ordered a pizza!");
+        }
+        else if(e.getSource() == hamburgerButton) {
+            System.out.println("You ordered a hamburger!");
+        }
+        else if(e.getSource() == hotdogButton) {
+            System.out.println("You ordered a hotdog");
         }
     }
     
