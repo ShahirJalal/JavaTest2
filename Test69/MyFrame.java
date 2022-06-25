395067ab46d59f02056924a835a4cedecd41120c
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-25 23:40:43 +0800

    Update MyFrame.java

--- Test69/MyFrame.java
+++ Test69/MyFrame.java
@@ -4,11 +4,15 @@ import javax.swing.*;
 
 public class MyFrame extends JFrame implements ActionListener {
 
-    MyFrame
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.pack();
+        this.setVisible(true);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
         
     }
     
