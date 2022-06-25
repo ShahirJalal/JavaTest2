159c9a686ebdd2ece2cfef9b6020dfee3beb5044
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-25 23:38:32 +0800

    Update MyFrame.java

--- Test69/MyFrame.java
+++ Test69/MyFrame.java
@@ -1,3 +1,15 @@
-public class MyFrame {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    MyFrame
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
